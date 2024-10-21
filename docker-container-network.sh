861eb06e19d05bf466cebcc632a2dad492382f69
M Hardi Raiz <raizhardi@gmail.com>
2024-10-21 21:27:51 +0700

    adjustment sintaks

--- docker-container-network.sh
+++ docker-container-network.sh
@@ -1,11 +1,16 @@
 
 docker network create --driver bridge mongonetwork
 
-docker container create --name mongodb --network mongonetwork --env MONGO_INITDB_ROOT_USERNAME=eko --env MONGO_INITDB_ROOT_PASSWORD=eko mongo:latest
+docker container create --name mongodb --network mongonetwork --env MONGO_INITDB_ROOT_USERNAME=hardi --env MONGO_INITDB_ROOT_PASSWORD=hardi mongo:latest
 
 docker image pull mongo-express:latest
 
-docker container create --name mongodbexpress --network mongonetwork --publish 8081:8081 --env ME_CONFIG_MONGODB_URL="mongodb://eko:eko@mongodb:27017/" mongo-express:latest
+# pattern sebelum definisi username dan password pada url
+docker container create --name mongodbexpress --network mongonetwork --publish 8081:8081 --env ME_CONFIG_MONGODB_URL="mongodb://username:pasword@namahost:27017/" mongo-express:latest
+docker container create --name mongodbexpress --network mongonetwork --publish 8081:8081 --env ME_CONFIG_MONGODB_URL="mongodb://hardi:hardi@mongodb:27017/" mongo-express:latest
+# jika ketika akses port diharuskan input username dan password
+# maka remove container yang dibuat sebelumnya dan buat ulang dengan sintaks berikut
+docker container create --name mongodbexpress --network mongonetwork --publish 8081:8081 --env ME_CONFIG_MONGODB_URL="mongodb://hardi:hardi@mongodb:27017/" --env ME_CONFIG_BASICAUTH_USERNAME="hardi" --env ME_CONFIG_BASICAUTH_PASSWORD="hardi" mongo-express:latest
 
 docker container start mongodb
 
--- docker-volume-backup.sh
+++ docker-volume-backup.sh
@@ -1,13 +1,15 @@
 
 docker container stop mongovolume
 
-docker container create --name nginxbackup --mount "type=bind,source=/Users/khannedy/Developments/YOUTUBE/belajar-docker-dasar/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" nginx:latest
+docker container create --name nginxbackup --mount "type=bind,source=/home/m-hardi-raiz/Program/belajar-docker-dasar/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" nginx:latest
 
 docker container start nginxbackup
 
 docker container exec -i -t nginxbackup /bin/bash
 
 tar cvf /backup/backup.tar.gz /data
+
+exit
 
 docker container stop nginxbackup
 
@@ -17,8 +19,9 @@ docker container start mongovolume
 
 docker image pull ubuntu:latest
 
+# Untuk eksekusi lebih ringkas gunakan 3 command dibawah
 docker container stop mongovolume
 
-docker container run --rm --name ubuntubackup --mount "type=bind,source=/Users/khannedy/Developments/YOUTUBE/belajar-docker-dasar/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" ubuntu:latest tar cvf /backup/backup-lagi.tar.gz /data
+docker container run --rm --name ubuntubackup --mount "type=bind,source=/home/m-hardi-raiz/Program/belajar-docker-dasar/backup,destination=/backup" --mount "type=volume,source=mongodata,destination=/data" ubuntu:latest tar cvf /backup/backup-lagi.tar.gz /data
 
 docker container start mongovolume
